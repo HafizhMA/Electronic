055ec8f0e38a205c077d8adbe347d8ce5fdd209d
HafizhMA <muhammadazhr29@gmail.com>
2024-11-09 20:12:00 +0700

    upload image to cloudinary

--- src/components/profile-component/EditProfile.jsx
+++ src/components/profile-component/EditProfile.jsx
@@ -1,17 +1,32 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 
 const EditProfile = () => {
     const [File, setFile] = useState();
 
-    const preset = import.meta.env.VITE_ClOUDINARY_CLOUD_NAME;
+    const preset = import.meta.env.VITE_CLOUDINARY_PRESET_NAME;
+    const cloudName = import.meta.env.VITE_ClOUDINARY_CLOUD_NAME;
 
-    const handleChangeFile = (e) => {
+    const handleChangeFile = async (e) => {
         const selectedFile = e.target.files[0]
         setFile(URL.createObjectURL(selectedFile));
 
         const formData = new FormData();
         formData.append('file', selectedFile);
-        formData.append('upload_preset',)
+        formData.append('upload_preset', preset);
+        formData.append('cloud_name', cloudName);
+
+        try {
+            const response = await axios.post(
+                `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+                formData
+            );
+            console.log('Image URL:', response.data);
+            alert('Image uploaded successfully!');
+        } catch (error) {
+            console.error('Error uploading image:', error);
+            alert('Failed to upload image.');
+        }
     }
 
 
@@ -28,7 +43,10 @@ const EditProfile = () => {
                         <input type="text" className='text-black' />
                     </div>
                     <div className='flex justify-between space-x-2 items-center'>
-                        <input type="file" onChange={handleChangeFile} accept='image/' className='rounded' />
+                        <input type="file"
+                            onChange={handleChangeFile}
+                            accept="image/*"
+                            className='rounded' />
                     </div>
                 </div>
             </form>
